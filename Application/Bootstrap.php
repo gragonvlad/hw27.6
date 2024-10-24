7af81b6038000991532616ffcc7e5adfe360cf7a
gragonvlad <gragonvlad@gmail.com>
2024-10-24 15:07:48 +0300

    --composer

--- Application/Bootstrap.php
+++ Application/Bootstrap.php
@@ -6,6 +6,6 @@ require_once 'Core/Model.php';
 require_once 'Core/View.php'; 
 require_once 'Core/Controller.php'; 
 require_once 'Core/Route.php'; 
-
+import $ from '../vendor/components/jquery/jquery.min.js';
 Route::start(); // запускаем маршрутизатор
 ?>
--- Application/Views/template_view.php
+++ Application/Views/template_view.php
@@ -7,7 +7,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<link rel="stylesheet" href="CSS/style.css">
-	<title>PHP-32</title>
+	<title></title>
 </head>
 
 <body>
@@ -19,6 +19,15 @@
 </body>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-<script src="/JS/script.js"></script>
+<script src='../../vendor/components/jquery/jquery.min.js' type="text/javascript">
 
+</script>
+<script  type="text/javascript">
+$(document).ready(function() {
+  $('button').click(function(event) {
+    event.preventDefault();
+    $("p").css("background-color", "yellow");
+  });
+});
+</script>
 </html>
